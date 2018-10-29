0af7c5ed04cb355d023e39e64ca482eac65a6167
Stephen <stepheny755@gmail.com>
2018-10-29 09:00:14 -0700

    new file:   SeadroneMotorController/SeaDrone-Smart-Thruster-Library-master.zip 	new file:   SeadroneMotorController/SeaDrone-Smart-Thruster-Library-master/.gitignore 	new file:   SeadroneMotorController/SeaDrone-Smart-Thruster-Library-master/LICENSE.txt 	new file:   SeadroneMotorController/SeaDrone-Smart-Thruster-Library-master/README.md 	new file:   SeadroneMotorController/SeaDrone-Smart-Thruster-Library-master/seadrone/__init__.py 	new file:   SeadroneMotorController/SeaDrone-Smart-Thruster-Library-master/seadrone/smart_thruster.py 	new file:   SeadroneMotorController/SeaDrone-Smart-Thruster-Library-master/thruster_advanced_example.py 	new file:   SeadroneMotorController/SeaDrone-Smart-Thruster-Library-master/thruster_basic_example.py 	new file:   SeadroneMotorController/code/Motor.cpp 	new file:   SeadroneMotorController/code/Motor.cpp.save 	new file:   SeadroneMotorController/code/Motor.h 	new file:   SeadroneMotorController/code/Motor.h.gch 	new file:   SeadroneMotorController/code/MotorReadTest 	new file:   SeadroneMotorController/code/a.out 	new file:   SeadroneMotorController/code/main.cpp 	new file:   SeadroneMotorController/code/test

---
+++ SeadroneMotorController/code/Motor.cpp
@@ -0,0 +1,93 @@
+#include <cstdio> 
+#include <cstdlib>
+#include <cerrno>
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <iostream>
+#include "Motor.h"
+
+std::string alarm_description[27];
+
+Motor::Values::Values(){
+
+	
+
+
+}
+
+Motor::Motor()
+{
+
+	std::string portname = "/dev/ttyUSB0";
+	std::cout << "start" << std::endl;
+	fd = open(portname.c_str(),O_RDWR);//|O_NOCTTY|O_NDELAY);
+	if(fd < 0){
+		printf("Error attaching SeaDrone Smart Motor Thruster on %s.\n", portname.c_str());
+		printf(" %d\n",fd);
+	}
+	std::cout << "connected port\n";
+	initializeInterface(B921600);
+	std::cout << "initiazlied interface\n";
+	readMotor();
+	std::cout << "read motor\n";
+}
+
+int Motor::initializeInterface(int speed){
+
+	struct termios tty;
+	memset(&tty,0,sizeof tty);
+	if(tcgetattr(fd,&tty)!=0){
+		printf("Error %d from tcgetattr.\n",errno);
+		return -1;
+	}
+	
+	cfsetspeed(&tty,speed);
+	
+	tty.c_cflag &= ~PARENB; //no parity
+	tty.c_cflag &= ~CSTOPB; //two stop bits
+	tty.c_cflag &= ~CSIZE; //
+	tty.c_cflag |= CS8; //eight bits
+
+	tty.c_cc[VMIN]	= 0;
+	tty.c_cc[VTIME] = 0.01;
+
+	if(tcsetattr(fd,TCSANOW,&tty)!=0){
+		printf("Error %d from tcgetattr.\n",errno);
+		return -1;
+	}
+	
+        alarm_description[0] = "No alarm";
+        alarm_description[16] = "Driver temperature too high";
+        alarm_description[18] = "Input voltage too high";
+        alarm_description[19] = "Input voltage too low";
+        alarm_description[20] = "Motor speed/acceleration too high";
+        alarm_description[21] = "Over current, payload is too high";
+        alarm_description[26] = "Alarm 0x1A, overcurrent";
+	return 0;
+}
+
+void Motor::readMotor(){
+
+	char c = 0;
+	int a = 0;
+	while(a<50){
+		read (fd,&c,1);
+		std::cout<<c;
+		a++;
+	}
+
+}
+
+std::string getAlarm(int code){
+}
+
+
+
+
+
+
+
---
+++ SeadroneMotorController/code/Motor.h
@@ -0,0 +1,46 @@
+#ifndef MOTOR_H
+#define MOTOR_H
+
+#include <string>
+
+using std::string;
+
+class Motor{
+
+public:
+	Motor();
+	
+private:
+
+	class Values{
+	public:
+		Values();
+	
+
+	private:
+		bool running;
+		bool is_on;
+		
+		int motors;
+		int has_alarm;
+		int alarm_code;
+
+		float rpm;
+		float target_rpm;
+		float current;
+		float voltage;
+		float driver_temperature;
+	
+	};
+
+	
+
+	int initializeInterface(int speed);
+	void readMotor();
+	
+
+	int fd;
+	char buf[100];
+};
+
+#endif
