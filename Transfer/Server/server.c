6fcd274c25ec1138aacb54c79f7765c0353d6d1b
Stephen Yang <stepheny755@gmail.com>
2018-11-11 01:10:51 +0000

    new file:   server.c

---
+++ Transfer/Server/server.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+void error(const char *er){
+	perror(msg);
+	exit(1);
+}
+
+int main(int argc, char *argv[]){
+
+	int sockfd, newsockfd, portno;
+
+
+}
