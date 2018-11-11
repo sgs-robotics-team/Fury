64063a31cc69ce1d2e1daabe3e81ec00a926eaaf
Stephen Yang <stepheny755@gmail.com>
2018-11-10 23:09:05 -0800

    Update main.c

--- Transfer/Server/main.c
+++ Transfer/Server/main.c
@@ -1,4 +1,14 @@
 #include "server.h"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+
 
 int main(void){
   
