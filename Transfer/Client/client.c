bf60696867168efe069afced8403f7490cb525aa
Stephen Yang <stepheny755@gmail.com>
2018-11-10 21:34:51 -0800

    Update client.c

--- Transfer/Client/client.c
+++ Transfer/Client/client.c
@@ -9,17 +9,13 @@
 #include <errno.h>
 #include <arpa/inet.h> 
 
+#define IPADDR 192.168.0.17
+
 int main(int argc, char *argv[])
 {
     int sockfd = 0, n = 0;
     char recvBuff[1024];
     struct sockaddr_in serv_addr; 
-
-    if(argc != 2)
-    {
-        printf("\n Usage: %s <ip of server> \n",argv[0]);
-        return 1;
-    } 
 
     memset(recvBuff, '0',sizeof(recvBuff));
     if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
@@ -33,7 +29,7 @@ int main(int argc, char *argv[])
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(5000); 
 
-    if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0)
+    if(inet_pton(AF_INET, IPADDR, &serv_addr.sin_addr)<=0)
     {
         printf("\n inet_pton error occured\n");
         return 1;
