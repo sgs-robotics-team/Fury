27295d71d0ecf5569f42aba7559ae81a36619f88
Stephen Yang <stepheny755@gmail.com>
2019-02-25 15:43:59 -0800

    deleted duplicated files

--- Daedalus-master/compass/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "Compass.h"
-#include <iostream>
-#include <unistd.h>
-int main(){
-	Compass c;
-	usleep(1000000);
-	std::cout << c.getHeading() << " " << c.getPitch() << " " << c.getRoll() << std::endl;
-}
--- Excalibur-master/New Compass/compassEdited.cpp
+++
@@ -1,3 +0,0 @@
-#include "Compass.h"
-#include <iostream>
-
