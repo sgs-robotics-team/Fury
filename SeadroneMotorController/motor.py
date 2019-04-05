317819544958f3e27f12d565871ff38eae0f3cb9
Stephen Yang <stepheny755@gmail.com>
2019-04-05 16:21:16 -0700

    updated motor.py

--- SeadroneMotorController/motor.py
+++ SeadroneMotorController/motor.py
@@ -2,6 +2,7 @@ import seadrone.smart_thruster as thrusters
 import threading
 import time
 import math
+import sys
 
 N_motors = 15
 port = '/dev/ttyS3'
@@ -39,12 +40,12 @@ class Motor:
                 motor_feedback += '{:5.2f}V '.format(self.m.voltage[id])
                 motor_feedback += '{:5.2f}C '.format(self.m.driver_temperature[id])
                 motor_feedback += 'Alarm: ' + self.m.get_alarm_description(id)
-                #print(motor_feedback)
+                print(motor_feedback)
                 #TODO: Transfer motor_feedback through ethernet again so the Jetson can figure out what to do because I don't :   ^)
                 if self.m.has_alarm[id]:
                     print("Auto-resetting motor alarm")
                     m.reset_alarm(id)
-            time.sleep(0.05) # wait 50ms after each printout
+            time.sleep(0.01) # wait 50ms after each printout
 
     def run(self): #TODO: Unnecessary function
         try:
@@ -52,15 +53,20 @@ class Motor:
                 for id in self.m.motors:
                     #self.m.target_rpm[id] = int(amplitude*math.sin(2*math.pi*(id+1)*frequency*time.time()))
                     #print(id)
-                    self.m.target_rpm[id] = 100#self.rpm[id]
+                    self.m.target_rpm[id] = 500#self.rpm[id]
                 time.sleep(0.01)
         except KeyboardInterrupt: # Program can be stopped pressing CTRL+C
             for id in self.m.motors:
                 self.m.target_rpm[id] = 0
+            self.stop()
             print("Shutting down smart thruster library...")
 
     def setRPM(self,id,speed):
         self.m.target_rpm[id]=speed
+        
+    def setAllRPM(self,speed):
+        for id in range(N_motors):
+            self.m.target_rpm[id]=speed
 
     def stopAll(self):
         for id in range(N_motors):
@@ -73,25 +79,16 @@ class Motor:
         self.stopAll()
         self.m.stop()
 
-    def getVoltage(self,id):
-        return self.m.voltage[id]
-
-    def getTemp(self,id):
-        return self.m.driver_temperature[id]
-
-    def getCurrent(self,id):
-        return self.m.current[id]
-
-    def getRpm(self,id):
-        return self.m.rpm[id]
+    def getVoltage(self,id): return self.m.voltage[id]
+    def getTemp(self,id): return self.m.driver_temperature[id]
+    def getCurrent(self,id): return self.m.current[id]
+    def getRpm(self,id): return self.m.rpm[id]
 
 if(__name__=="__main__"):
     mo = Motor()
-    print(mo.getRpm(1))
-    print(mo.getCurrent(1))
-    print(mo.getVoltage(1))
-    print(mo.getTemp(1))
-    mo.setRPM(1,500)
-    while True:
+    mo.setAllRPM(500)
+    try:
         mo.run()
+    except KeyboardInterrupt:
+        sys.exit(0)
     mo.stop()
